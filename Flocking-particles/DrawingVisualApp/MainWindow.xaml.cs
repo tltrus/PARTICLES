1493e94c2ecd008b702124cb338fa8a346209bbc
tltrus <77125487+tltrus@users.noreply.github.com>
2024-04-03 21:32:14 +0400

    up

---
+++ Flocking-particles/DrawingVisualApp/MainWindow.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace DrawingVisualApp
+{
+    /// <summary>
+    /// Coding Challenge #124 - Flocking Simulation
+    /// https://thecodingtrain.com/CodingChallenges/124-flocking-boids.html
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        System.Windows.Threading.DispatcherTimer timerDraw, timerControl;
+        public static Random rnd = new Random();
+        public static int width, height;
+        Vector2D mouse = new Vector2D();
+
+        DrawingVisual visual;
+        DrawingContext dc;
+
+        List<Particle> particles = new List<Particle>();
+
+        public static double alignSlider, cohesionSlider, separationSlider;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            visual = new DrawingVisual();
+
+            width = (int)g.Width;
+            height = (int)g.Height;
+
+            timerDraw = new System.Windows.Threading.DispatcherTimer();
+            timerDraw.Tick += new EventHandler(timerDrawTick);
+            timerDraw.Interval = new TimeSpan(0, 0, 0, 0, 10);
+
+            timerControl = new System.Windows.Threading.DispatcherTimer();
+            timerControl.Tick += new EventHandler(timerControlTick);
+            timerControl.Interval = new TimeSpan(0, 0, 0, 0, 10);
+
+
+            for (int i = 0; i < 60; ++i)
+            {
+                var x = rnd.Next(10, width - 10);
+                var y = rnd.Next(10, height - 10);
+                particles.Add(new Particle(x, y, 1.5f, Brushes.White));
+            }
+
+            timerControl.Start();
+            timerDraw.Start();
+        }
+
+        private void timerDrawTick(object sender, EventArgs e) => Drawing();
+        private void timerControlTick(object sender, EventArgs e) => Control();
+
+        private void Control()
+        {
+            foreach (var p in particles)
+            {
+                p.Flock(particles);
+                p.Update();
+            }
+        }
+
+        private void Drawing()
+        {
+            g.RemoveVisual(visual);
+            using (dc = visual.RenderOpen())
+            {
+                foreach(var p in particles)
+                {
+                    p.Show(dc);
+                    p.Edges();
+                }
+                dc.Close();
+                g.AddVisual(visual);
+            }
+        }
+
+        private void sbAlign_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e) => alignSlider = e.NewValue;
+        private void sbSeparation_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e) => separationSlider = e.NewValue;
+        private void sbCohesion_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e) => cohesionSlider = e.NewValue;
+        private void g_MouseMove(object sender, MouseEventArgs e)
+        {
+            mouse.X = e.GetPosition(g).X;
+            mouse.Y = e.GetPosition(g).Y;
+        }
+    }
+}
---
+++ Flocking-particles/DrawingVisualApp/Particle.cs
@@ -0,0 +1,156 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Media;
+
+
+namespace DrawingVisualApp
+{
+    class Particle
+    {
+        public Vector2D position;
+        public Vector2D velocity;
+        public Vector2D acceleration;
+        public Brush brush;
+        public float radius; // radius
+        public double maxForce = 0.2;
+        public int maxSpeed = 4;
+
+        public Particle(double x, double y, float radius, Brush brush)
+        {
+            position = new Vector2D(x, y);
+            velocity = new Vector2D();
+            acceleration = new Vector2D();
+            maxForce = 0.2;
+            maxSpeed = 4;
+            this.brush = brush;
+            this.radius = radius;
+        }
+
+        public Vector2D Align(List<Particle> particles)
+        {
+            int perceptionRadius = 50;
+            Vector2D steering = new Vector2D(0, 0);
+            int total = 0;
+            foreach (var other in particles)
+            {
+                double d = Vector2D.Dist(position, other.position);
+
+                if (other != this && d < perceptionRadius)
+                {
+                    steering.Add(other.velocity);
+                    total++;
+                }
+            }
+            if (total > 0)
+            {
+                steering.Div(total);
+                steering.SetMag(maxSpeed);
+                steering.Sub(velocity);
+                steering.Limit(maxForce);
+            }
+            return steering;
+        }
+
+        public Vector2D Cohesion(List<Particle> particles)
+        {
+            int perceptionRadius = 50;
+            Vector2D steering = new Vector2D(0, 0);
+            int total = 0;
+            foreach (var other in particles)
+            {
+                double d = Vector2D.Dist(position, other.position);
+
+                if (other != this && d < perceptionRadius)
+                {
+                    steering.Add(other.position);
+                    total++;
+                }
+            }
+            if (total > 0)
+            {
+                steering.Div(total);
+                steering.Sub(position);
+                steering.SetMag(maxSpeed);
+                steering.Sub(velocity);
+                steering.Limit(maxForce);
+            }
+            return steering;
+        }
+
+        public Vector2D Separation(List<Particle> particles)
+        {
+            int perceptionRadius = 50;
+            Vector2D steering = new Vector2D(0, 0);
+            int total = 0;
+            foreach (var other in particles)
+            {
+                double d = Vector2D.Dist(position, other.position);
+
+                if (other != this && d < perceptionRadius)
+                {
+                    var diff = Vector2D.Sub(position, other.position);
+                    diff.Div(d * d);
+                    steering.Add(diff);
+                    total++;
+                }
+            }
+            if (total > 0)
+            {
+                steering.Div(total);
+                steering.SetMag(maxSpeed);
+                steering.Sub(velocity);
+                steering.Limit(maxForce);
+            }
+            return steering;
+        }
+
+        public void Flock(List<Particle> particles)
+        {
+            Vector2D alignment = Align(particles);
+            Vector2D cohesion = Cohesion(particles);
+            Vector2D separation = Separation(particles);
+
+            alignment.Mult(MainWindow.alignSlider);
+            cohesion.Mult(MainWindow.cohesionSlider);
+            separation.Mult(MainWindow.separationSlider);
+
+            acceleration.Add(alignment);
+            acceleration.Add(cohesion);
+            acceleration.Add(separation);
+        }
+
+        public void Edges()
+        {
+            if (position.X > MainWindow.width)
+            {
+                position.X = 0;
+            }
+            else if (position.X < 0)
+            {
+                position.X = MainWindow.width;
+            }
+            if (position.Y > MainWindow.height)
+            {
+                position.Y = 0;
+            }
+            else if (position.Y < 0)
+            {
+                position.Y = MainWindow.height;
+            }
+        }
+
+        public void Update()
+        {
+            position.Add(velocity);
+            velocity.Add(acceleration);
+            velocity.Limit(maxSpeed);
+            acceleration.Mult(0);
+        }
+
+        public void Show(DrawingContext dc)
+        {
+            Point p = new Point(position.X, position.Y);
+            dc.DrawEllipse(Brushes.White, null, p, radius, radius);
+        }
+    }
+}
