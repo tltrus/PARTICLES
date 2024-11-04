74d005b1a565aeab7333ac511703991d537ddfe2
tltrus <77125487+tltrus@users.noreply.github.com>
2024-11-04 20:03:48 +0400

    up

---
+++ Fire-drawingVisual/DrawingVisualApp/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+
+
+namespace DrawingVisualApp
+{
+    public partial class MainWindow : Window
+    {
+        System.Windows.Threading.DispatcherTimer timerDraw;
+        public static Random rnd = new Random();
+        public static int width, height;
+
+        DrawingVisual visual;
+        DrawingContext dc;
+
+        List<Particle> particles = new List<Particle>();
+
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            visual = new DrawingVisual();
+
+            width = (int)g.Width;
+            height = (int)g.Height;
+
+            timerDraw = new System.Windows.Threading.DispatcherTimer();
+            timerDraw.Tick += new EventHandler(timerDrawTick);
+            timerDraw.Interval = new TimeSpan(0, 0, 0, 0, 30);
+
+            timerDraw.Start();
+        }
+
+        private void timerDrawTick(object sender, EventArgs e) => Drawing();
+
+        private void Drawing()
+        {
+            g.RemoveVisual(visual);
+            using (dc = visual.RenderOpen())
+            {
+                for (int i = 0; i < 5; ++i)
+                {
+                    particles.Add(new Particle());
+                }
+
+                for (int i = particles.Count-1; i >= 0; i--)
+                {
+                    particles[i].Update();
+                    particles[i].Show(dc);
+                    if (particles[i].Finished())
+                        particles.RemoveAt(particles.IndexOf(particles[i]));
+                }
+                dc.Close();
+                g.AddVisual(visual);
+            }
+        }
+
+        private void g_MouseMove(object sender, MouseEventArgs e)
+        {
+        }
+    }
+}
---
+++ Fire-drawingVisual/DrawingVisualApp/Particle.cs
@@ -0,0 +1,43 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace DrawingVisualApp
+{
+    class Particle
+    {
+        double x;
+        double y;
+        double vx;
+        double vy;
+        float alpha = 240f;
+
+        public Particle()
+        {
+            x = MainWindow.width/2;
+            y = MainWindow.height - 10;
+
+
+            vx = -1 + MainWindow.rnd.NextDouble() * 3;
+            vy = MainWindow.rnd.Next(-5, 0);
+        }
+
+        public bool Finished()
+        {
+            return alpha < 1;
+        }
+
+        public void Update()
+        {
+            x += vx;
+            y += vy;
+            alpha -= 5;
+        }
+
+        public void Show(DrawingContext dc)
+        {
+            Point p = new Point(x, y);
+            Brush brush = new SolidColorBrush(Color.FromArgb((byte)alpha, 255, 0, 0));
+            dc.DrawEllipse(brush, null, p, 8, 8);
+        }
+    }
+}
