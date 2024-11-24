0231dc7fe0cb3e97a0cb27500cb73a668e7dd93d
tltrus <77125487+tltrus@users.noreply.github.com>
2024-11-24 18:42:47 +0400

    ElectricCharges

---
+++ Electric-charges/Charge.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace WpfApp
+{
+    internal class Charge
+    {
+        public int x, y;
+        private int dir_x, dir_y;
+        public double q; // заряд
+        int r;
+        Color color;
+
+        public Charge(double q, int x, int y, Color color)
+        {
+            this.q = q;
+            this.x = x;
+            this.y = y;
+            this.color = color;
+            r = (int)Math.Abs(q) * 10;
+
+            dir_x = MainWindow.rnd.Next(-1, 2);
+            dir_y = MainWindow.rnd.Next(-1, 2);
+        }
+
+        public void Draw(WriteableBitmap wb)
+        {
+            wb.FillEllipseCentered(x, y, r, r, color);
+        }
+
+        public void Update()
+        {
+            x += dir_x;
+            y += dir_y;
+        }
+    }
+}
---
+++ Electric-charges/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace WpfApp
+{
+    public partial class MainWindow : Window
+    {
+        System.Windows.Threading.DispatcherTimer timer;
+        public static Random rnd = new Random();
+        WriteableBitmap wb;
+        int imgWidth, imgHeight;
+        List<Charge> charges;
+        int destiny = 27;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Init();
+        }
+
+        private void Init()
+        {
+            imgWidth = (int)image.Width; imgHeight = (int)image.Height;
+
+            wb = new WriteableBitmap(imgWidth, imgHeight, 96, 96, PixelFormats.Bgra32, null);
+            image.Source = wb;
+
+            timer = new System.Windows.Threading.DispatcherTimer();
+            timer.Tick += new EventHandler(timerTick);
+            timer.Interval = new TimeSpan(0, 0, 0, 0, 50);
+
+            charges = new List<Charge>();
+            charges.Add(new Charge(1, 100, 100, Colors.White));
+            charges.Add(new Charge(-2, 400, 200, Colors.White));
+            charges.Add(new Charge(1, 250, 370, Colors.White));
+
+            timer.Start();
+        }
+
+        private void timerTick(object sender, EventArgs e) => Draw();
+
+        void Draw()
+        {
+            wb.Clear(Colors.Black);
+
+            for(int y = 0; y < imgHeight; y++) 
+            {
+                for (int x = 0; x < imgWidth; x++)
+                {
+                    double u = 0;
+
+                    foreach (var charge in charges)
+                    {
+                        var ri = Math.Sqrt((x - charge.x) * (x - charge.x) + (y - charge.y) * (y - charge.y)) / 500;
+                        if (ri != 0) u += charge.q / ri;
+                    }
+
+                    var a = (int)(u * destiny);
+
+                    if (a % destiny == 0)
+                    {
+                        wb.SetPixel(x, y, Colors.White);
+                    }
+                }
+
+            }
+
+            foreach (var charge in charges)
+            {
+                charge.Draw(wb);
+                charge.Update();
+            }
+        }
+    }
+}
