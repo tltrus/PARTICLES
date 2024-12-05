9334159c1b26648462e3d30e5983c4a6ebd72cf5
tltrus <77125487+tltrus@users.noreply.github.com>
2024-12-05 22:01:36 +0400

    up

---
+++ Falling-water/DrawingVisualApp/MainWindow.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace DrawingVisualApp
+{
+    public partial class MainWindow : Window
+    {
+        System.Windows.Threading.DispatcherTimer timer, timer2;
+        public static Random rnd = new Random();
+        public static double width, height;
+        Point pMouse;
+        int oldCell;
+
+        DrawingVisual visual;
+        DrawingContext dc;
+
+        Map Tank;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            visual = new DrawingVisual();
+
+            width = g1.Width;
+            height = g1.Height;
+
+            timer = new System.Windows.Threading.DispatcherTimer();
+            timer.Tick += new EventHandler(timerTick);
+            timer.Interval = new TimeSpan(0, 0, 0, 0, 50);
+
+            timer2 = new System.Windows.Threading.DispatcherTimer();
+            timer2.Tick += new EventHandler(timer2Tick);
+            timer2.Interval = new TimeSpan(0, 0, 0, 0, 300);
+
+            Init();
+
+            timer.Start();
+        }
+
+        void Init()
+        {
+            Tank = new Map(80, 80, 5);
+        }
+
+
+        private void Draw()
+        {
+            g1.RemoveVisual(visual);
+
+            using (dc = visual.RenderOpen())
+            {
+                Tank.Draw(dc);
+
+                dc.Close();
+                g1.AddVisual(visual);
+            }
+        }
+
+        private void timerTick(object sender, EventArgs e) => Draw();
+        private void timer2Tick(object sender, EventArgs e) => Tank.SetTo(1, Tank.cols / 2, 1);
+        private void btnTimer_Click(object sender, RoutedEventArgs e) => timer2.Start();
+        private void Canvas_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            pMouse = e.GetPosition(g1);
+            oldCell = Tank.GetCellindex(pMouse);
+
+            Tank.SetWall(pMouse);
+            Draw();
+        }
+        private void Canvas_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            pMouse = e.GetPosition(g1);
+            Draw();
+        }
+        private void canvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                pMouse = e.GetPosition(g1);
+
+                int index = Tank.GetCellindex(pMouse);
+
+                if (index != oldCell)
+                {
+                    Tank.SetWall(pMouse);
+                    Draw();
+                    oldCell = index;
+                }
+            }
+        }
+    }
+}
---
+++ Falling-water/DrawingVisualApp/Map.cs
@@ -0,0 +1,192 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace DrawingVisualApp
+{
+    internal class Map
+    {
+        public int rows;
+        public int cols;
+        int cellsize;
+        public int[,] map;
+        int[,] next_map;
+
+        public Map(int rows = 40, int cols = 40, int cellsize = 10) 
+        { 
+            this.rows = rows;
+            this.cols = cols;
+            this.cellsize = cellsize;
+
+            map = new int[rows, cols];
+            next_map = new int[rows, cols];
+
+            // Set walls and floor
+            for (int i = 0; i < rows; ++i)
+            {
+                map[i, cols - 1] = -2;    // right wall = -2
+                map[i, 0] = -2;           // right wall = -2
+            }
+            for (int j = 0; j < cols; ++j)
+            {
+                map[rows - 1, j] = -2;    // floor = -2
+            }
+        }
+
+        public int GetCellindex(Point p)
+        {
+            int X = (int)p.X / cellsize;
+            int Y = (int)p.Y / cellsize;
+
+            return (Y * cols) + X;
+        }
+
+        public void SetWall(Point p)
+        {
+            int X = (int)p.X / cellsize;
+            int Y = (int)p.Y / cellsize;
+
+            if ((X >= cols) || (Y >= rows)) return; // if click out of field
+
+            map[Y, X] = (map[Y, X] == -2) ? 0 : -2; // trigger set\reset cell wall
+        }
+
+        public void SetTo(int row, int col, int value) => map[row, col] = value;
+
+        private int SearchEmptyCell(int row_index, int start_index)
+        {
+            int counter1 = start_index;
+            int counter2 = start_index;
+
+            bool flag = true;
+            counter1--;
+            counter2++;
+
+            while (flag)
+            {
+                if (counter1 >= 0)
+                {
+                    if (map[row_index, counter1] == 1)
+                    {
+                        counter1--;
+                    }
+                    else if (map[row_index, counter1] == 0)
+                    {
+                        return counter1;
+                    }
+                }
+
+                if (counter2 < cols)
+                {
+                    if (map[row_index, counter2] == 1)
+                    {
+                        counter2++;
+                    }
+                    else if (map[row_index, counter2] == 0)
+                    {
+                        return counter2;
+                    }
+                }
+
+                if (map[row_index, counter1] == -2 && map[row_index, counter2] == -2) flag = false;
+            }
+
+            return -5;
+        }
+
+        public void Draw(DrawingContext dc)
+        {
+            next_map = new int[rows, cols];
+
+            for (int i = 1; i < rows - 1; i++)
+            {
+                for (int j = 1; j < cols - 1; j++)
+                {
+
+                    if (map[i, j] == -2)
+                    {
+                        next_map[i, j] = map[i, j];
+                    }
+
+                    Brush brush = Brushes.White;
+                    if (map[i, j] == 0)
+                    {
+                        brush = Brushes.White;
+                    }
+                    else
+                    if (map[i, j] == 1)
+                    {
+                        brush = Brushes.Blue;
+                    }
+                    else
+                    if (map[i, j] == -2)
+                    {
+                        brush = Brushes.Red;
+                    }
+
+                    Rect rect = new Rect()
+                    {
+                        X = j * cellsize,
+                        Y = i * cellsize,
+                        Width = cellsize,
+                        Height = cellsize
+                    };
+                    dc.DrawRectangle(brush, new Pen(Brushes.Black, 0.1), rect);
+
+                    //      |_|
+                    //       0
+                    if (map[i, j] == 1 && map[i + 1, j] == 0)
+                    {
+                        next_map[i + 1, j] = 1;
+                    }
+                    else
+                    //      |_|
+                    //     0   0
+                    if (map[i, j] == 1 && map[i + 1, j + 1] == 0 && map[i + 1, j - 1] == 0)
+                    {
+                        if (MainWindow.rnd.NextDouble() < 0.5)
+                            next_map[i + 1, j + 1] = 1;
+                        else
+                            next_map[i + 1, j - 1] = 1;
+                    }
+                    else
+                    //      |_|
+                    //     0   !0
+                    if (map[i, j] == 1 && map[i + 1, j - 1] == 0 && map[i + 1, j + 1] != 0)
+                    {
+                        next_map[i + 1, j - 1] = 1;
+                    }
+                    else
+                    //      |_|
+                    //    !0   0
+                    if (map[i, j] == 1 && map[i + 1, j + 1] == 0 && map[i + 1, j - 1] != 0)
+                    {
+                        next_map[i + 1, j + 1] = 1;
+                    }
+                    else
+                    //      |_|
+                    //     1   1
+                    if (map[i, j] == 1 && map[i + 1, j + 1] == 1 && map[i + 1, j - 1] == 1)
+                    {
+                        int calc_index = SearchEmptyCell(i + 1, j);
+
+                        if (calc_index != -5)
+                            map[i + 1, calc_index] = 1;
+                        else
+                            next_map[i, j] = map[i, j];
+                    }
+                    else
+                    if (map[i, j] == 1)
+                    {
+                        next_map[i, j] = map[i, j];
+                    }
+                }
+            }
+
+            for (int i = 1; i < rows - 1; i++)
+                for (int j = 1; j < cols - 1; j++)
+                {
+                    map[i, j] = next_map[i, j];
+                }
+        }
+    }
+}
